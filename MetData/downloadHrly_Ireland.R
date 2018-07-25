30729c7d03b3f9d36ac41cab9072f7b40ab1c898
GMcG <29357294+gavanmcgrath@users.noreply.github.com>
2018-07-25 19:02:05 +0800

    Add files via upload

---
+++ MetData/downloadHrly_Ireland.R
@@ -0,0 +1,133 @@
+#Pull Met Eireann rainfall data
+cwd <- getwd()
+setwd("D:/Data/AHPC/climate_Hourly")
+met.ie_hrlySites <- c(
+#Johnstown Castle
+"https://cli.fusio.net/cli/climate_data/webdata/hly1775.zip",
+#Mulingar
+"https://cli.fusio.net/cli/climate_data/webdata/hly875.zip",
+#Gurteen
+"https://cli.fusio.net/cli/climate_data/webdata/hly1475.zip",
+#Markree
+"https://cli.fusio.net/cli/climate_data/webdata/hly1275.zip",
+#Mt_Dillon
+"https://cli.fusio.net/cli/climate_data/webdata/hly1975.zip",
+#Dunsany
+"https://cli.fusio.net/cli/climate_data/webdata/hly1375.zip",
+#Belmullet
+"https://cli.fusio.net/cli/climate_data/webdata/hly2375.zip",
+#Claremorris
+"https://cli.fusio.net/cli/climate_data/webdata/hly2175.zip",
+#Knock_Airport
+"https://cli.fusio.net/cli/climate_data/webdata/hly4935.zip",
+#Newport
+"https://cli.fusio.net/cli/climate_data/webdata/hly1175.zip",
+#Valentia_Observatory
+"https://cli.fusio.net/cli/climate_data/webdata/hly2275.zip",
+#Athenry
+"https://cli.fusio.net/cli/climate_data/webdata/hly1875.zip",
+#Mace_Head
+"https://cli.fusio.net/cli/climate_data/webdata/hly275.zip",
+#Casement
+"https://cli.fusio.net/cli/climate_data/webdata/hly3723.zip",
+#Dublin_Airport
+"https://cli.fusio.net/cli/climate_data/webdata/hly532.zip",
+#Phoenix_Park
+"https://cli.fusio.net/cli/climate_data/webdata/hly175.zip",
+#Fineer
+"https://cli.fusio.net/cli/climate_data/webdata/hly2075.zip",
+#Malin_Head
+"https://cli.fusio.net/cli/climate_data/webdata/hly1575.zip",
+#Cork_Airport
+"https://cli.fusio.net/cli/climate_data/webdata/hly3904.zip",
+#Moore_Park
+"https://cli.fusio.net/cli/climate_data/webdata/hly575.zip",
+#Roches_Point
+"https://cli.fusio.net/cli/climate_data/webdata/hly1075.zip",
+#Sherkin_Island
+"https://cli.fusio.net/cli/climate_data/webdata/hly775.zip",
+#Shannon_Airport
+"https://cli.fusio.net/cli/climate_data/webdata/hly518.zip",
+#Ballyhaise
+"https://cli.fusio.net/cli/climate_data/webdata/hly675.zip",
+#Oak_Park
+"https://cli.fusio.net/cli/climate_data/webdata/hly375.zip"
+)
+dest.files <- strsplit(met.ie_hrlySites,split = "https://cli.fusio.net/cli/climate_data/webdata/")
+dest.files<- unlist(dest.files)
+dest.files <- dest.files[dest.files!= ""]
+dest.files <- paste0("D:/Data/AHPC/climate_Hourly/", dest.files)
+sapply(1:length(dest.files), FUN = function(x) {
+  download.file(met.ie_hrlySites[x], 
+                destfile = dest.files[x],
+                method = "wininet")
+  zdir <- strsplit(dest.files[x],split=".zip")[[1]]
+  unzip(dest.files[x], exdir = zdir)
+  unlink(dest.files[x])
+})
+
+
+wrking.dir <- getwd()
+dirs <- list.dirs()
+n <- length(met.ie_hrlySites)
+hrly.database <- data.frame(station = vector("character", length = n), 
+                            height = vector("numeric", length =n), 
+                            lat = vector("numeric", length =n), 
+                            long = vector("numeric", length =n),
+                            start = vector("character", length =n),
+                            end = vector("character", length =n),
+                            folder = vector("character", length =n),
+                            end = vector("numeric", length =n),
+                            stringsAsFactors = FALSE)
+weather <- vector("list", length =n)
+for (i in 2:length(dirs)){
+  setwd(dirs[i])
+  climfile <- list.files(".", pattern = ".csv")
+  dat <- readLines(climfile)
+  stn.id <- trimws(strsplit(dat[1], split = ":")[[1]][2])
+  stn.height <- as.numeric(strsplit(trimws(strsplit(dat[2], split = ":")[[1]][2]), split = " ")[[1]][1])
+  latlong <- as.numeric(strsplit(gsub("[[:alpha:,:punct:]]*", "", dat[3]), split = ",")[[1]])
+  pos.blanks <- pos <- which(dat == "")
+  print(paste0("Station id: ",stn.id))
+  print(paste0("Lat: ",latlong[1], " Lon: ", latlong[2]))
+  hrly.database$station[i-1] <- stn.id
+  hrly.database$height[i-1] <- stn.height
+  hrly.database$lat[i-1] <- latlong[1]
+  hrly.database$long[i-1] <- latlong[2]
+  hrly.database$folder[i-1] <- dirs[i]
+  hrly.database$d[i-1] <- i
+  
+  variables <- c()
+  unit.var <- c()
+  for (j in (pos[2]+1):(pos[3]-1)){
+    ln1 <- dat[j]
+    varbl <- strsplit(ln1,split = ":")[[1]]
+    variables <- c(variables,varbl[1] )
+    unt <- trimws(strsplit(varbl[2], split = "-")[[1]][2])
+    unit.var <- c(unit.var , unt)
+  }
+  climdat <- read.table(climfile, skip = pos[3], header = TRUE,
+                        colClasses = c("character",rep("numeric", length(variables)-1)),
+                        fill = TRUE, sep = ",")
+  dates <- as.POSIXct( climdat[,1], format = "%d-%b-%Y %H:%M")
+  start <- as.character(as.Date(dates[1]))
+  end <- as.character(as.Date(dates[length(dates)]))
+  hrly.database$start[i-1] <- start
+  hrly.database$end[i-1] <- end
+  
+  climdat[,1] <- dates
+  dtrng <- range(dates, na.rm=TRUE)
+  print(paste0("Date Range. From: ", dtrng[1], " To: ",dtrng[2] ))
+  print("")
+  attr(climdat, "units") <- unit.var
+  attr(climdat, "variables") <- variables
+  weather[[i-1]] <- climdat
+  
+  setwd(wrking.dir)
+}
+saveRDS(list(stations = hrly.database, 
+             weather = weather), 
+        file = "hrldatabase.rds")
+setwd(cwd)
+
+
---
+++ MetData/voroniDailyRain.R
@@ -0,0 +1,80 @@
+voroniRain <- function(start.date,end.date, daily = TRUE, plotVoroni = FALSE){
+  #Input Require
+  #start.date a character vector of length 1 'yyyy-mm-dd'
+  #end.date a character vector of length 1 'yyyy-mm-dd'
+  
+  #Input Optional:
+  #daily a boolean (default = TRUE) whether to load available daily stations (TRUE)
+  # or whether to load available hourly stations
+  
+  #Value:
+  #A spatial polygons dataframe
+  # A Thesian polygon of active daily rain gauges in the database
+  # Attributes of the polygons includes:
+  #   the staion name
+  #   the station height
+  #   the lat and long coordinates of the station
+  #   the start and end dates of the daily record (note this may include some NAs)
+  #   the folder location where daily rain data is held (relative to D:/Data/AHPC/climate_Daily)
+  
+  #Requires:
+  #The presence of D:/Data/AHPC/climate_Daily/dlydatabase.rds as created by
+  #downloadDaily_Ireland.R
+  
+  require(rgdal)
+  require(sp)
+  library(dismo)
+  require(raster)
+  
+  #corine <- readOGR("D:/Data/GIS/Corine/clc12_IE/clc12_IE.shp")
+  #require(rgeos)
+  #ireland <- gIntersection(corine, corine)
+  #writeOGR(ireland, dsn = "D:/Data/GIS/Corine/Ireland.shp", layer = "Ireland.shp", driver = "ESRI Shapefile")
+  #ireland <- readOGR("D:/Data/GIS/Corine/Ireland.shp")
+  if (daily){
+    dailyDB <- readRDS("D:/Data/AHPC/climate_Daily/dlydatabase.rds")
+  } else {
+    dailyDB <- readRDS("D:/Data/AHPC/climate_Hourly/hrldatabase.rds")
+  }
+  pos2strip <- which(dailyDB$stations$long == 0)
+  if (length(pos2strip) > 0 ){
+    dailyDB$stations <- dailyDB$stations[-pos2strip,]
+  }
+  stationDaily <- SpatialPointsDataFrame(coords = dailyDB$stations[,c("long","lat")], 
+                                         data = dailyDB$stations,
+                                         proj4string = CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"))
+  pos.start <- which(as.Date(stationDaily$start) <= as.Date(start.date))
+  pos.end <-which(as.Date(stationDaily$end) >= as.Date(end.date))
+  
+  if (length(pos.start) == 0) stop("Start Date not in Range")
+  if (length(pos.end) == 0) stop("End Date not in Range")
+  pos.indates <- intersect(pos.start,pos.start)
+  if (length(pos.indates) == 0) stop("No data in date range")
+  
+  stationDailyTrimmed <- stationDaily[pos.indates,]
+  #irelnd <- spTransform(irelnd, CRSobj = CRS(proj4string(stationDaily)))
+  
+  #Thesian Polygon from coordinates of points
+  xys <- coordinates( stationDailyTrimmed)
+  vor <- voronoi(xys, ext = c())
+  proj4string(vor) <- "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"
+  #append station data to each polygon
+  vordat <- over(vor,stationDailyTrimmed)
+  vor$station <- vordat$station
+  vor$height <- vordat$height
+  vor$lat <- vordat$lat
+  vor$long <- vordat$long
+  vor$start <- vordat$start
+  vor$end <- vordat$end
+  vor$folder <- vordat$folder
+  
+  #to do intersect the voroni with a map of ireland  
+  #vor2 <- intersect(vor,ireland)
+  
+  if (plotVoroni){
+    plot(vor)
+    points(stationDailyTrimmed)
+  }
+  
+  return(vor)
+}
